e62bdc5379520d59933cda00d8fc9515d0a8cc87
dmitriyZakharov-dev <forsocials.mail@gmail.com>
2020-03-30 00:21:59 +0300

    init

--- src/components/templates/main.js
+++ src/components/templates/main.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Grid, Container, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
 	},
 	paper: {
+		marginTop: theme.spacing(1),
 		padding: theme.spacing(3),
 		color: theme.palette.text.secondary,
 	},
 	content: {
 		flexGrow: 1,
 		padding: theme.spacing(3),
+	},
+	title: {
+		fontSize: 18,
 	},
 }));
 
@@ -23,12 +32,20 @@ export default function () {
 		<Container>
 			<Grid container spacing={2} className={classes.content}>
 				<Grid item xs={8}>
-					<Paper className={classes.paper}>
-						1
-					</Paper>
+					<Card>
+						<CardContent>
+							<Typography className={classes.title} color="success">
+								Рабочая область
+							</Typography>
+						</CardContent>
+					</Card>
 				</Grid>
 
 				<Grid item xs={4}>
+					<Paper className={classes.paper}>
+						1
+					</Paper>
+
 					<Paper className={classes.paper}>
 						1
 					</Paper>
